feadc4972aac5f8ccdb166fdd09eebb336378185
HariAgus <haryagus97@gmail.com>
2022-11-03 13:42:10 +0700

    First commit and add dependecies

---
+++ app/src/main/java/haw/bmaajp/groceriesapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package haw.bmaajp.groceriesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import haw.bmaajp.groceriesapp.ui.theme.GroceriesAppTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GroceriesAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+
+}
