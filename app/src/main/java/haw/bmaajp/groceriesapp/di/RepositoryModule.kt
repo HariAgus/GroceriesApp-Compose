6f023657067a6488d250649d12a9bbb651f30256
HariAgus <haryagus97@gmail.com>
2022-12-12 14:27:41 +0700

    Fix onboarding with state

--- app/src/main/java/haw/bmaajp/groceriesapp/di/RepositoryModule.kt
+++ app/src/main/java/haw/bmaajp/groceriesapp/di/RepositoryModule.kt
@@ -7,7 +7,11 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import haw.bmaajp.groceriesapp.data.repository.OnBoardingOperationImpl
+import haw.bmaajp.groceriesapp.data.repository.Repository
 import haw.bmaajp.groceriesapp.domain.repository.OnBoardingOperations
+import haw.bmaajp.groceriesapp.domain.usecase.UseCases
+import haw.bmaajp.groceriesapp.domain.usecase.readonboarding.ReadOnBoardingUseCase
+import haw.bmaajp.groceriesapp.domain.usecase.saveonboarding.SaveOnBoardingUseCase
 import javax.inject.Singleton
 
 @Module
@@ -19,5 +23,14 @@ object RepositoryModule {
     fun provideDataStoreOperation(
         @ApplicationContext context: Context
     ): OnBoardingOperations = OnBoardingOperationImpl(context = context)
+
+    @Provides
+    @Singleton
+    fun provideUseCase(repository: Repository): UseCases {
+        return UseCases(
+            saveOnBoardingUseCase = SaveOnBoardingUseCase(repository),
+            readOnBoardingUseCase = ReadOnBoardingUseCase(repository)
+        )
+    }
 
 }
--- app/src/main/java/haw/bmaajp/groceriesapp/presentation/screen/splash/SplashScreen.kt
+++ app/src/main/java/haw/bmaajp/groceriesapp/presentation/screen/splash/SplashScreen.kt
@@ -1,28 +1,59 @@
 package haw.bmaajp.groceriesapp.presentation.screen.splash
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import haw.bmaajp.groceriesapp.R
+import haw.bmaajp.groceriesapp.navigation.Screen
 import haw.bmaajp.groceriesapp.ui.theme.Green
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(navController: NavHostController) {
-    Splash()
+fun SplashScreen(
+    navController: NavHostController,
+    splashViewModel: SplashViewModel = hiltViewModel()
+) {
+    val onBoardingIsCompleted by splashViewModel.onBoardingIsCompleted.collectAsState()
+    val scale = remember { Animatable(0f) }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.8f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                }
+            )
+        )
+        delay(1200L)
+        navController.popBackStack()
+
+        if (onBoardingIsCompleted) navController.navigate(Screen.Home.route)
+        else navController.navigate(Screen.OnBoarding.route)
+    }
+
+    Splash(scale = scale.value)
 }
 
 @Composable
 fun Splash(
+    scale: Float,
     modifier: Modifier = Modifier
 ) {
     Box(
@@ -32,7 +63,9 @@ fun Splash(
         contentAlignment = Alignment.Center
     ) {
         Image(
-            modifier = Modifier.padding(all = 64.dp),
+            modifier = Modifier
+                .scale(scale)
+                .padding(all = 64.dp),
             painter = painterResource(id = R.drawable.img_logo_app),
             contentDescription = stringResource(R.string.logo_app)
         )
@@ -42,5 +75,5 @@ fun Splash(
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    Splash()
+    Splash(scale = 0f)
 }
